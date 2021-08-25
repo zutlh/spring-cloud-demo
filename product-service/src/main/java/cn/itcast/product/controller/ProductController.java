0516f275b3c23200445496f9f3e9273ae6f3e4f3
zutlh <liuhao19971212@163.com>
2021-08-25 17:59:20 +0800

    daily update

--- product-service/src/main/java/cn/itcast/product/controller/ProductController.java
+++ product-service/src/main/java/cn/itcast/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package cn.itcast.product.controller;
 import cn.itcast.product.entity.Product;
 import cn.itcast.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -17,9 +18,16 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Value("${server.port}")
+    private String port;
+    @Value("${spring.cloud.client.ip-address}")
+    private String ip;
+
+
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public Product findById(@PathVariable Long id){
         Product product = productService.findById(id);
+        product.setProductName("访问的服务地址"+ip+":"+port);
         return product;
     }
 
